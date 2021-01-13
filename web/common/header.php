c8cb865a97cb8bb29c77e0ffbd236183dd5d6c93
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-01-12 18:27:05 -0700

    added common folder with footer and header files. Added images folder with a logo file. Added some code to the homepage for testing.

---
+++ web/common/header.php
@@ -0,0 +1,3 @@
+<div id="pageHeader">
+    <img src="/web/images/logo.png" alt="my website Logo" id="siteLogo">
+</div>
---
+++ web/view/homepage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8" /> <!-- Define the character coding: preferred encoding for e-mail and web pages -->
+        <meta http-equiv="x-ua-compatible" content="ie=edge" /> <!-- Latest Internet Explorer rendering mode (optional) -->
+        <meta name="viewport" content="width=device-width, initial-scale=1" /> <!-- Define the width of the viewport: optimize for mobile and prevent zoom/horizontal scroll -->
+        <meta name="author" content="Jacob MacDonald" />
+        <meta name="description" content="Home page for my site" />
+
+        <title>Home | Jake MacDonald</title>
+        <link rel="stylesheet" media="screen" href="/web/css/main.css"/>
+
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <main>
+            <h1>TEST2</h1>
+        </main>
+    </body>
+</html>
