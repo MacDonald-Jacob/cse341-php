f6bfd6bb51697373ae4b2b7ad1ebaf061b0271b8
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-01-16 11:20:38 -0700

    changed paths

--- web/view/homepage.php
+++ web/view/homepage.php
@@ -1,10 +1,10 @@
 
 <?php $currentPage = 'homePage'; ?>
-<?php require $_SERVER['DOCUMENT_ROOT'] . '/cs313-php/web/common/head.php'; ?>
-<?php require $_SERVER['DOCUMENT_ROOT'] . '/cs313-php/web/common/navBar.php'; ?>
+<?php require '../common/head.php'; ?>
+<?php require '../common/navBar.php'; ?>
 <body>
     <main>
         <h1>Home Page</h1>
         <h2>Coming Soon!</h2>
     </main>
-<?php require $_SERVER['DOCUMENT_ROOT'] . '/cs313-php/web/common/footer.php'; ?>
+<?php require '../common/footer.php'; ?>
