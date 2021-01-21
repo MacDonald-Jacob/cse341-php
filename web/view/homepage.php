bd7d9e4c923974862f26ceec3994e5e0ea042eb5
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-01-20 17:29:58 -0700

    added button link to homepage

--- web/view/homepage.php
+++ web/view/homepage.php
@@ -5,6 +5,9 @@
 <body>
     <main>
         <h1>Home Page</h1>
-        <h2>Coming Soon!</h2>
+        <br>
+        <div class="list-group">
+            <a href="../teamActivities/activityOne/view/Home.html" class="btn btn-info" role="button">Team Activity 1</a>
+        </div>
     </main>
 <?php require '../common/footer.php'; ?>
