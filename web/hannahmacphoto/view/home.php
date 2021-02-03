761784e14f2ac8cebc343142e78174c00e44ed6d
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-03 14:06:28 -0700

    testing url for common head with heroku

--- web/hannahmacphoto/view/home.php
+++ web/hannahmacphoto/view/home.php
@@ -1,14 +1,17 @@
 <?php $currentPage = 'Home'; ?>
-<?php require '../commonH/head.php'; ?>
+<?php require $_SERVER['DOCUMENT_ROOT'] . '/cs313-php/web/hannahmacphoto/commonH/head.php'; ?>
 
 <body>
     <header>
     </header>
-    <!-- Navbar --><?php require '../commonH/navBar.php'; ?>
+    Navbar
+    <?php 
+    // require '../commonH/navBar.php'; ?>
     <main>
         <h1>Home</h1>
 
     </main>
-    <!-- Footer --><?php require '../commonH/footer.php'; ?>
+    <!-- Footer<?php 
+    // require '../commonH/footer.php'; ?> -->
 </body>
 
