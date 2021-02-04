43a1df7ce140681e69247105fbf1b4539874d79b
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-03 20:22:19 -0700

    added more

--- web/teamActivities/activityThree/view/search.php
+++ web/teamActivities/activityThree/view/search.php
@@ -3,7 +3,14 @@
 require_once '../library/connections.php';
 if (($_SERVER['REQUEST_METHOD'] == 'POST'))
 {
-    $rows = $db->query('SELECT * FROM team05.scriptures WHERE book LIKE "%'.$_POST["book"].'%"');
+    // $rows = $db->query('SELECT * FROM team05.scriptures WHERE book LIKE "%'.$_POST["book"].'%"');
+    $sql = 'SELECT * FROM team05.scriptures WHERE book LIKE "%:book%"';
+	$stmt = $db->prepare($sql);
+	$stmt->bindValue(':book', $_POST['book'], PDO::PARAM_STR);
+	$stmt->execute();
+	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	$stmt->closeCursor();
+
     foreach ($rows as $row){
         echo "<div><a href=\"/scriptureDetails.php?id=" . $row["id"] . "\"><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b></a></div>";
     }
