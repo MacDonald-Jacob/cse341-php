61f5a9ddf212dc8bf9ad1f7887df9c02289ebcec
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-03 20:34:06 -0700

    d

--- web/teamActivities/activityThree/view/scripturesDetails.php
+++ web/teamActivities/activityThree/view/scripturesDetails.php
@@ -1,8 +1,10 @@
 <?php
 require_once '../library/connections.php';
 
-$row = $db -> query('SELECT * FROM team05.scriptures WHERE id='.$_GET["id"]);
-echo "<div><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b> - \"" . $row['content'] . "\"</div>";
+$rows = $db -> query('SELECT * FROM team05.scriptures WHERE id='.$_GET["id"]);
+foreach ($rows as $row){
+    echo "<div><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b> - \"" . $row['content'] . "\"</div>";
+}
 
 
 
