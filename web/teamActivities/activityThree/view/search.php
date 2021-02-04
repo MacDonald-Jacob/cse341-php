20d66c8db178de0aa4a7a1487df9d1e12cb4909b
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-03 20:51:53 -0700

    d

--- web/teamActivities/activityThree/view/search.php
+++ web/teamActivities/activityThree/view/search.php
@@ -6,15 +6,15 @@ if (($_SERVER['REQUEST_METHOD'] == 'POST'))
     print_r($_POST);
     // $rows = $db->query('SELECT * FROM team05.scriptures WHERE book LIKE "%'.$_POST["book"].'%"');
     try {
-    $sql = 'SELECT * FROM team05.scriptures WHERE book = :book';
+    $sql = 'SELECT * FROM team05.scriptures WHERE book LIKE :book';
 	$stmt = $db->prepare($sql);
-	$stmt->bindValue(':book', $_POST['book'], PDO::PARAM_STR);
+	$stmt->bindValue(':book', '%'.$_POST['book'].'%', PDO::PARAM_STR);
 	$stmt->execute();
 	$rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	$stmt->closeCursor();
 
     foreach ($rows as $row){
-        echo "<div><a href=\"scriptureDetails.php?id=" . $row["id"] . "\"><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b></a></div>";
+        echo "<div><a href=\"scripturesDetails.php?id=" . $row["id"] . "\"><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b></a></div>";
     }
 } catch (PDOException $e) {echo $e;}
 }
