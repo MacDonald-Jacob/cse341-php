6b719eb87da8d73fbe64bb4b5a6c1d2325373c08
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-03 20:05:23 -0700

    added pages

--- web/teamActivities/activityThree/view/index.php
+++ web/teamActivities/activityThree/view/index.php
@@ -1,7 +1,8 @@
 <?php
 //Get the database connection file
 require_once '../library/connections.php';
-
+echo "<h1>Scripture Resources</h1>";
 echo $db;
+
 
 ?>
---
+++ web/teamActivities/activityThree/view/search.php
@@ -0,0 +1,19 @@
+
+<?php
+require_once '../library/connections.php';
+if (($_SERVER['REQUEST_METHOD'] == 'POST'))
+{
+    $rows = $db->query('SELECT * FROM team05.scriptures WHERE book LIKE "%'.$_POST["book"].'%"');
+    foreach ($rows as $row){
+        echo "<div><a href=\"/scriptureDetails.php?id=" . $row["id"] . "\"><b>" . $row['book'] . " " . $row['chapter'] . ":" . $row['verse'] . "</b></a></div>";
+    }
+}
+?>
+
+<html>
+<form>
+<input type='text' name='book'>
+<input type='submit' value='submit'>
+</from>
+</html>
+
