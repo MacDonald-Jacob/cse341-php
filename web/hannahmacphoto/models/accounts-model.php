78bfbd103e116cdfc723b22194fb039e0e256100
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-04 18:48:44 -0700

    test

--- web/hannahmacphoto/models/accounts-model.php
+++ web/hannahmacphoto/models/accounts-model.php
@@ -4,7 +4,7 @@
     // Get user data based on userName
     function getUser($userName){
         $db = hannahmacphotoConnect();
-        $sql = 'SELECT userID, userName, userPassword FROM hmphoto.user WHERE userID = :userID';
+        $sql = 'SELECT userID, userName, userPassword FROM hmphoto.user WHERE userName = :userName';
         $stmt = $db->prepare($sql);
         $stmt->bindValue(':userName', $userName, PDO::PARAM_STR);
         $stmt->execute();
