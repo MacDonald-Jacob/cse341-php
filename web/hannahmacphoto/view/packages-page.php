93c8dc8983239a85205c4378d8d0f6dea88b3c7b
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-05 17:12:31 -0700

    test

--- web/hannahmacphoto/view/packages-page.php
+++ web/hannahmacphoto/view/packages-page.php
@@ -10,11 +10,13 @@
                 $packages = $db->query('SELECT p.packageName, p.packageImg, p.packagePrice, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID', PDO::FETCH_ASSOC); 
                 $dv = '<ul id="package-display">';
                 foreach ($packages as $package) {
+                    echo $package['packageName'];
+                    echo $package["packagePrice"];
                     $currency = number_format($package['packagePrice']);
                     $dv .= '<li>';
                     $dv .= "<div id='packageImg'>";
                     // $dv .= "<a href='/hannahmacphoto/packages/?action=individualPackage.php' title='View the individual package page.'>";
-                    $dv .= "<img src='$package[packageImg]' alt='Image of $package[packageName] on hannahmacphotography.com'>";
+                    $dv .= "<img src='$package[packageImg]' alt='Image of" . $package['packageName'] . "on hannahmacphotography.com'>";
                     $dv .= "</div>";
                     $dv .= '<hr id="packageID">';
                     $dv .= "<h2>$package[packageName]</h2>";
