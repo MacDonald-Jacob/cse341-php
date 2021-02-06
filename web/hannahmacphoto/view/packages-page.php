7ba58d4b5f9c31b02d477a090dbb40a73f60cb79
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-06 12:51:52 -0700

    test

--- web/hannahmacphoto/view/packages-page.php
+++ web/hannahmacphoto/view/packages-page.php
@@ -18,24 +18,24 @@
 
 
 
-                $packages = $db->query('SELECT p.*, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID ORDER BY p.mediaid, p.packageid'); 
-                $dv = '<ul id="package-display">';
-                foreach ($packages as $package) {
-                    $currency = number_format($package['packageprice']);
-                    $dv .= '<li>';
-                    $dv .= "<div id='packageImg'>";
-                    // $dv .= "<a href='/hannahmacphoto/packages/?action=individualPackage.php' title='View the individual package page.'>";
-                    $dv .= "<img src='$package[packageimg]' alt='Image of" . $package['packagename'] . "on hannahmacphotography.com' id='packageImg'>";
-                    $dv .= "</div>";
-                    $dv .= '<hr id="packageID">';
-                    $dv .= "<h2>$package[packagename]</h2>";
-                    // $dv .= "</a>";
-                    $dv .= "<span class='formatCurrency'>$currency</span>";
-                    $dv .= '</li>';
-                }
-                    $dv .= '</ul>';
+                // $packages = $db->query('SELECT p.*, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID ORDER BY p.mediaid, p.packageid'); 
+                // $dv = '<ul id="package-display">';
+                // foreach ($packages as $package) {
+                //     $currency = number_format($package['packageprice']);
+                //     $dv .= '<li>';
+                //     $dv .= "<div id='packageImg'>";
+                //     // $dv .= "<a href='/hannahmacphoto/packages/?action=individualPackage.php' title='View the individual package page.'>";
+                //     $dv .= "<img src='$package[packageimg]' alt='Image of" . $package['packagename'] . "on hannahmacphotography.com' id='packageImg'>";
+                //     $dv .= "</div>";
+                //     $dv .= '<hr id="packageID">';
+                //     $dv .= "<h2>$package[packagename]</h2>";
+                //     // $dv .= "</a>";
+                //     $dv .= "<span class='formatCurrency'>$currency</span>";
+                //     $dv .= '</li>';
+                // }
+                //     $dv .= '</ul>';
 
-                    echo $dv;
+                //     echo $dv;
 
 
 
@@ -65,18 +65,18 @@
 
 
                     // $dv1 = '<ul id="package-display">';
-                    foreach ($packages1 as $package) {
+                    foreach ($packages1 as $package1) {
                         $dv1 = '<div class="container">';
                             $dv1 .= '<div class="row">';
                                 $dv1 .= '<div class="col-sm-3">';
-                                    $currency = number_format($package['packageprice']);
+                                    $currency = number_format($package1['packageprice']);
                                     // $dv1 .= '<li>';
                                     $dv1 .= "<div id='packageImg'>";
                                     // $dv .= "<a href='/hannahmacphoto/packages/?action=individualPackage.php' title='View the individual package page.'>";
-                                    $dv1 .= "<img class='img-responsive' src='$package[packageimg]' alt='Image of" . $package['packagename'] . "on hannahmacphotography.com' id='packageImg'>";
+                                    $dv1 .= "<img class='img-responsive' src='$package1[packageimg]' alt='Image of" . $package1['packagename'] . "on hannahmacphotography.com' id='packageImg'>";
                                     $dv1 .= "</div>";
                                     $dv1 .= '<hr id="packageID">';
-                                    $dv1 .= "<h2>$package[packagename]</h2>";
+                                    $dv1 .= "<h2>$package1[packagename]</h2>";
                                     // $dv1 .= "</a>";
                                     $dv1 .= "<span class='formatCurrency'>$currency</span>";
                                     // $dv1 .= '</li>';
