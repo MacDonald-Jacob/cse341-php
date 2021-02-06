27e3f8fcf13d4c6007faedb996718ecd438997d9
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-06 12:36:12 -0700

    style

--- web/hannahmacphoto/view/packages-page.php
+++ web/hannahmacphoto/view/packages-page.php
@@ -6,7 +6,6 @@
         <?php require '../commonH/navBar.php'; ?>
         <main>
             <h1>Packages</h1>
-<img src="" alt="">
                 <?php
                 // $statement = $db->prepare('SELECT p.packageName, p.packageImg, p.packagePrice, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID');
                 // $statement->execute();
@@ -37,6 +36,59 @@
                     $dv .= '</ul>';
 
                     echo $dv;
+
+
+
+
+
+
+
+
+                    $packages = $db->query('SELECT p.*, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID WHERE p.mediaid = 1 ORDER BY p.packageid'); 
+                    // $dv1 = '<ul id="package-display">';
+                    // foreach ($packages as $package) {
+                    //     $currency = number_format($package['packageprice']);
+                    //     $dv .= '<li>';
+                    //     $dv .= "<div id='packageImg'>";
+                    //     // $dv .= "<a href='/hannahmacphoto/packages/?action=individualPackage.php' title='View the individual package page.'>";
+                    //     $dv .= "<img src='$package[packageimg]' alt='Image of" . $package['packagename'] . "on hannahmacphotography.com' id='packageImg'>";
+                    //     $dv .= "</div>";
+                    //     $dv .= '<hr id="packageID">';
+                    //     $dv .= "<h2>$package[packagename]</h2>";
+                    //     // $dv .= "</a>";
+                    //     $dv .= "<span class='formatCurrency'>$currency</span>";
+                    //     $dv .= '</li>';
+                    // }
+                    //     $dv .= '</ul>';
+    
+                    //     echo $dv;
+
+
+                    $dv1 = '<ul id="package-display">';
+                    foreach ($packages as $package) {
+                        $dv1 = '<div class="container">';
+                            $dv1 .= '<div class="row">';
+                                $dv1 .= '<div class="col-sm-3">';
+                                    $currency = number_format($package['packageprice']);
+                                    $dv1 .= '<li>';
+                                    $dv1 .= "<div id='packageImg'>";
+                                    // $dv .= "<a href='/hannahmacphoto/packages/?action=individualPackage.php' title='View the individual package page.'>";
+                                    $dv1 .= "<img class='img-responsive' src='$package[packageimg]' alt='Image of" . $package['packagename'] . "on hannahmacphotography.com' id='packageImg'>";
+                                    $dv1 .= "</div>";
+                                    $dv1 .= '<hr id="packageID">';
+                                    $dv1 .= "<h2>$package[packagename]</h2>";
+                                    // $dv1 .= "</a>";
+                                    $dv1 .= "<span class='formatCurrency'>$currency</span>";
+                                    $dv1 .= '</li>';
+                                $dv1 .= '</div>';
+                            $dv1 .= '</div>';
+                        $dv1 .= '</div>';
+                    }
+                    $dv1 .= '</ul>';
+
+                    echo $dv1;
+
+
             ?>
         </main>
         <?php require '../commonH/footer.php'; ?>
