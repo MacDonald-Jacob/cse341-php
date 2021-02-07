5eacf182d49d4fda6c570930b00e058f2afa714d
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-06 17:18:05 -0700

    home image

--- web/hannahmacphoto/view/home.php
+++ web/hannahmacphoto/view/home.php
@@ -5,7 +5,8 @@
         <?php require '../hannahmacphoto/commonH/header.php'; ?>
         <?php require '../hannahmacphoto/commonH/navBar.php'; ?>
         <main>
-            <h1>Home</h1>
+            <h1>Welcome</h1>
+            <img src="../imagesH/home.jpg" alt="home bride image">
         </main>
         <?php require '../hannahmacphoto/commonH/footer.php'; ?>
     </body>
