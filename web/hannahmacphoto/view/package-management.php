7503d7d5a88e12baab6847789d013fa5d872f1c4
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-09 14:58:57 -0700

    test

--- web/hannahmacphoto/view/package-management.php
+++ web/hannahmacphoto/view/package-management.php
@@ -1,10 +1,15 @@
 <?php
-    // Check to see if logged in. If not send to php main controller
+    // Check to see if logged in. If not send to main controller
     if(!$_SESSION['loggedin']){
         header('Location: /hannahmacphoto/index.php');
     }
-?><?php $currentPage = 'Package Management'; ?>
-    <?php require '../commonH/head.php'; ?>
+    $currentPage = 'Package Management'; 
+    
+    //Query
+    $packages = $db->query('SELECT p.*, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID ORDER BY p.packageid'); 
+
+    
+    ?><?php require '../commonH/head.php'; ?>
 
     <body>
         <?php require '../commonH/header.php'; ?>
@@ -12,14 +17,12 @@
         <main>
             <h1>Package Management</h1>
             <a href= '../packages/?action=add-package' title='View the add package page'>Add Package</a>
-
+            <h2> Update Packages</h2>
             <?php
-                echo "$addPackage";
-                $packages = $db->query('SELECT p.*, m.service FROM hmphoto.packages p LEFT JOIN hmphoto.media m ON p.mediaID = m.mediaID ORDER BY p.packageid'); 
-                echo "<h2> Update Packages</h2>";
                 foreach ($packages as $package) {
-                    // echo "<div>$package[packagename]</div>";
-                    echo "<a href= '../packages/?action=update-package' title='View the update package page'>$package[packagename]</a><br>";
+                    $packageID = $package['packageid'];
+                    $packageName = $package['packageName'];
+                    echo "<a href= '../packages/?action=update-package' title='View the update package page'>$packageName</a><br>";
                 }
             ?>
         </main>
