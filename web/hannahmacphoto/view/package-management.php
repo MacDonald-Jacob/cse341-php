4d7038cb0b50d3c53cd98df799879fd7cdcd8432
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-09 17:11:27 -0700

    test

--- web/hannahmacphoto/view/package-management.php
+++ web/hannahmacphoto/view/package-management.php
@@ -22,7 +22,7 @@
                 foreach ($packages as $package) {
                     $packageID = $package['packageid'];
                     $packageName = $package['packagename'];
-                    echo "<a href= '/view/update-package.php?package-id=$packageID' title='View the update package page'>$packageName</a><br>";
+                    echo "<a href= '/hannahmacphoto/view/update-package.php?package-id=$packageID' title='View the update package page'>$packageName</a><br>";
                 }
             ?>
         </main>
