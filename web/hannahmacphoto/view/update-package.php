40812ac67bd8287e8c008793a2e9cf876385d56c
MacDonald-Jacob <46459062+MacDonald-Jacob@users.noreply.github.com>
2021-02-10 14:33:11 -0700

    form style

--- web/hannahmacphoto/view/update-package.php
+++ web/hannahmacphoto/view/update-package.php
@@ -33,43 +33,43 @@
             <h1>Updage <?php echo $packageName?> Package</h1>
 
             <form action="/hannahmacphoto/packages/" method="post" id="addPackageForm">
-                <label class="required" for="packagename">Package Name</label>
+                <label class="required" for="packagename">Package Name:</label><br>
                 <input type="text" id="packagename" name="packagename" size="26" 
                 <?php 
                 if(isset($packageName)){
                     echo "value='$packageName'";
                 }  
                 ?> required><br>
-                <label class="required" for="packageimg">Image Path</label>
+                <label class="required" for="packageimg">Image Path:</label><br>
                 <input type="text" id="packageimg" name="packageimg" size="26" placeholder="/hannahmacphoto/imagesH/bridal.jpg" 
                 <?php 
                 if(isset($packageImg)){
                     echo "value='$packageImg'";
                 }  
                 ?> required><br>
-                <label class="required" for="packageprice">Package Price</label>
+                <label class="required" for="packageprice">Package Price:</label><br>
                 <input type="text" id="packageprice" name="packageprice" size="26"
                 <?php 
                 if(isset($packagePrice)){
                     echo "value='$packagePrice'";
                 }  
                 ?> required><br>
-                <label class="required" for="packagehours">Package Hours</label>
+                <label class="required" for="packagehours">Package Hours:</label><br>
                 <input type="text" id="packagehours" name="packagehours" size="26" 
                 <?php 
                 if(isset($packageHours)){
                     echo "value='$packageHours'";
                 }  
                 ?> required><br>
-                <label class="required" for="packagelocationcount">Number of Locations</label>
-                <input type="number" id="packagelocationcount" name="packagelocationcount" min="1" 
+                <label class="required" for="packagelocationcount">Number of Locations:</label><br>
+                <input type="number" id="packagelocationcount" name="packagelocationcount" min="1" size="26"
                 <?php 
                 if(isset($packageLocationCount)){
                     echo "value='$packageLocationCount'";
                 }  
                 ?> required><br>
                 <label class="required" for="packagedescription">Description</label><br>
-                <textarea id="packagedescription" name="packagedescription" rows="5" cols="50" form="addPackageForm" required><?php
+                <textarea id="packagedescription" name="packagedescription" rows="5" cols="26" form="addPackageForm" required><?php
                 if(isset($packageDescription)){
                      echo htmlentities($packageDescription, ENT_QUOTES);
                 }?></textarea><br><br>
